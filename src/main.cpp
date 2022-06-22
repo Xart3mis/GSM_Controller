de35340ac47fce0685488a745af3b74d16ae6127
Xart3mis <ihavemanywaifus@protonmail.com>
2022-06-22 03:55:09 +0000

    seif look at code pls

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
-#include <SoftwareSerial.h>
+// #include <ArduinoJson.h>
+#include <NeoSWSerial.h>
 
 #define API_KEY "pk.1eb53d351789d96db350712fa4983c80"
 #define LED_PIN 5
@@ -10,9 +10,7 @@
 #define ASCII_A_VALU 65
 #define ASCII_F_VALU 70
 
-StaticJsonDocument<JSON_OBJECT_SIZE(48)> doc;
-
-SoftwareSerial mySerial(10, 11);
+NeoSWSerial mySerial(10, 11);
 
 String MSG = "";
 String PHONE = "";
@@ -31,13 +29,8 @@ void get_loc();
 
 void setup()
 {
-  Serial.begin(9600);
-  mySerial.begin(9600);
-
-  while (!Serial || !mySerial)
-    flash_led(30, 2);
-
-  timed([]{flash_led(30, 1);}, 1000);
+  Serial.begin(38400);
+  mySerial.begin(38400);
 
   Serial.println(F("Initializing..."));
   delay(1000);
@@ -125,18 +118,20 @@ void loop()
 
 void updateSerial()
 {
-  delay(Serial.getTimeout() + mySerial.getTimeout());
+  delay(mySerial.getTimeout());
   while (Serial.available())
   {
     mySerial.write(Serial.read());
   }
   while (mySerial.available())
   {
-    notif = mySerial.readString();
+    notif = mySerial.readStringUntil('\n');
     notif.trim();
     notif.toLowerCase();
     Serial.println(notif);
   }
+
+  mySerial.flush();
 }
 
 void flash_led(int de, int x)
@@ -162,35 +157,35 @@ void send_SMS(String msg)
   mySerial.write(26);
 }
 
-
-unsigned int HexStringToUInt(char const* hexstring)
+unsigned int HexStringToUInt(char const *hexstring)
 {
-    unsigned int result = 0;
-    char const *c = hexstring;
-    char thisC;
+  unsigned int result = 0;
+  char const *c = hexstring;
+  char thisC;
 
-    while( (thisC = *c) != NULL )
+  while ((thisC = *c) != NULL)
+  {
+    unsigned int add;
+    thisC = toupper(thisC);
+
+    result <<= 4;
+
+    if (thisC >= ASCII_0_VALU && thisC <= ASCII_9_VALU)
+      add = thisC - ASCII_0_VALU;
+    else if (thisC >= ASCII_A_VALU && thisC <= ASCII_F_VALU)
+      add = thisC - ASCII_A_VALU + 10;
+    else
     {
-        unsigned int add;
-        thisC = toupper(thisC);
-
-        result <<= 4;
-
-        if( thisC >= ASCII_0_VALU &&  thisC <= ASCII_9_VALU )
-            add = thisC - ASCII_0_VALU;
-        else if( thisC >= ASCII_A_VALU && thisC <= ASCII_F_VALU)
-            add = thisC - ASCII_A_VALU + 10;
-        else
-        {
-            Serial.print("Unrecognised hex character ");
-            Serial.println(thisC);
-        }
-
-        result += add;
-        ++c;
+      Serial.print("Unrecognised hex character ");
+      Serial.println(thisC);
+      return 0;
     }
 
-    return result;
+    result += add;
+    ++c;
+  }
+
+  return result;
 }
 
 void get_loc()
@@ -205,62 +200,91 @@ void get_loc()
     updateSerial();
   } while (notif.indexOf("ok") < 0);
 
-  Serial.setTimeout(5000);
-  mySerial.setTimeout(15000);
+  mySerial.flush();
 
   mySerial.println(F("AT+CNETSCAN"));
-  updateSerial();
+  delay(30000);
 
-  Serial.setTimeout(1000);
-  mySerial.setTimeout(1000);
+  int buf_idx = 0;
+  char buf[320] = {'\0'};
 
+  while (mySerial.available())
+  {
+    char x = tolower(mySerial.read());
+    buf[buf_idx++] = x;
+    Serial.write(x);
+  }
+
+  buf[buf_idx] = '\n';
   Serial.println();
 
-  String shit[16] = {"\0"};
+  notif = String(buf);
+  notif.trim();
+  Serial.println(notif);
+  String shit[35] = {"\0"};
+
   int t = 0;
   int r = 0;
 
   for (uint16_t i = 0; i < notif.length(); i++)
   {
-    if (notif.charAt(i) == '\n')
+    if (notif.charAt(i) == ',')
     {
       shit[t] = notif.substring(r, i);
       r = (i + 1);
       t++;
     }
   }
+
   notif = "";
-  delay(350);
+
   // operator:"mobinil",mcc:602,mnc:01,rxlev:47,cellid:6c17,arfcn:60,lac:8184,bsic:12
 
+  String MCC, MNC, CID, LAC;
   int MCCs[16] = {0};
   int MNCs[16] = {0};
   int CIDs[16] = {0};
   int LACs[16] = {0};
 
-  // int MCCsMNCsCIDsLACs[16*4];
-
-  for (int i = 0; i < 16; i++)
+  for (int i = 0; i < 35; i++)
   {
-    if (shit[i].length() > 64)
+    if (shit[i].length() > 5 && shit[i].indexOf(":") > 0)
     {
+      shit[i].trim();
       Serial.println(shit[i]);
-      String MCC, MNC, CID, LAC;
 
-      MCC = shit[i].substring(shit[i].indexOf(",mcc:") + 5, shit[i].indexOf(",mnc"));
-      MNC = shit[i].substring(shit[i].indexOf(",mnc:") + 5, shit[i].indexOf(",rxlev"));
-      CID = shit[i].substring(shit[i].indexOf(",cell") + 8, shit[i].indexOf(",ar"));
-      LAC = shit[i].substring(shit[i].indexOf(",lac:") + 5, shit[i].indexOf(",bsic"));
+      if (shit[i].indexOf("mcc") >= 0)
+      {
+        MCC = shit[i].substring(4);
+        MCCs[i] = MCC.toInt();
+        Serial.print(MCC);
+        Serial.print(" ");
+      }
+      if (shit[i].indexOf("mnc") >= 0)
+      {
+        MNC = shit[i].substring(4);
+        MNCs[i] = MNC.toInt();
+        Serial.print(MNC);
+        Serial.print(" ");
+      }
+      if (shit[i].indexOf("cellid") >= 0) // seif pls pls
+      {
+        CID = shit[i].substring(3);
+        char temp[8] = {'\0'};
+        for (uint16_t i = 0; i < CID.length(); i++)
+          temp[i] = CID[i];
 
-      Serial.print(MCC);
-      Serial.print(" ");
-      Serial.print(MNC);
-      Serial.print(" ");
-      Serial.print(HexStringToUInt(CID.c_str()));
-      Serial.print(" ");
-      Serial.print(HexStringToUInt(LAC.c_str()));
-      Serial.println();
-
+        CIDs[i] = HexStringToUInt(temp);
+        Serial.print(CID);
+        Serial.print(" ");
+      }
+      if (shit[i].indexOf("lac") >= 0)
+      {
+        LAC = shit[i].substring(4);
+        LACs[i] = HexStringToUInt(LAC.c_str());
+        Serial.print(LAC);
+      }
+      Serial.println("\n");
     }
   }
 
@@ -272,21 +296,7 @@ void get_loc()
       idx++;
   }
 
-  JsonObject root = doc.to<JsonObject>();
-  root["token"] = API_KEY;
-  root["mcc"] = MCCs[0];
-  root["mnc"] = MNCs[0];
-
-  JsonArray array = root.createNestedArray("cells");
-
-  for (int i = 0; i < idx; i++)
-  {
-    JsonObject obj = array.createNestedObject();
-    obj["radio"] = "gsm";
-    obj["mcc"] = MCCs[i];
-  }
-
-  serializeJsonPretty(root, Serial);
+  Serial.println(idx);
 }
 
 void parse_SMS()
